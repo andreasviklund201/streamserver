5033620deb55ad937b7d43a6a0fad45fd9831d16
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-03 03:55:38 +0100

    edited
    edited files and folders

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if (( $EUID != 0 )); then
+  echo 'This script only works when run as root!'
+  exit
+fi
+apt-get -y install `cat package.php`
+mv Ubuntu-20.04-fs_overlay.tar.bz2 /
+export MOVEBACKTO=`pwd`
+cd /
+tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
+mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
+cd "$MOVEBACKTO"
+systemctl enable nginx
+systemctl restart nginx
+echo 'Install completed, check the README for next steps'
