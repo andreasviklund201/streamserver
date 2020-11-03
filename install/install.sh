325de0419047f1ffbc1d5b2f92f7294fcdd213b4
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-03 06:44:10 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,5 +9,9 @@ cd "$MOVEBACKTO"
 systemctl enable nginx
 systemctl restart nginx
 add-apt-repository -y ppa:certbot/certbot
-
+mkdir /root/install/wordpress
+cd /root/install/wordpress
+wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+sudo tar -xzvf /tmp/wordpress.tar.gz -C /var/www
+cp /var/www/wordpress/* /var/www/html
 echo 'Install completed, installing SSL'
