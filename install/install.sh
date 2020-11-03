0e6c8cc4e31e257fa22f2c3d6167ee5b87887d57
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-03 06:50:00 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,12 +6,12 @@ cd /
 tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
 mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
 cd "$MOVEBACKTO"
+add-apt-repository -y ppa:certbot/certbot
 systemctl enable nginx
 systemctl restart nginx
-add-apt-repository -y ppa:certbot/certbot
 mkdir /root/install/wordpress
 cd /root/install/wordpress
 wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
 sudo tar -xzvf /tmp/wordpress.tar.gz -C /var/www
 cp -r /var/www/wordpress/* /var/www/html
-echo 'Install completed, installing SSL'
+echo 'Install completed, remember to create database for wordpress'
