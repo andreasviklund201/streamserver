86e11fded0abff529f682b395f9ea8d5321acea6
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-03 05:15:01 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt-get -y install `cat package.txt`
-mv Ubuntu-20.04-fs_overlay.tar.bz2 /
+sudo apt-get -y install `cat package.txt`
+sudo mv Ubuntu-20.04-fs_overlay.tar.bz2 /
 export MOVEBACKTO=`pwd`
 cd /
-tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
-mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
+sudo tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
+sudo mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
 cd "$MOVEBACKTO"
-systemctl enable nginx
-systemctl restart nginx
+sudo systemctl enable nginx
+sudo systemctl restart nginx
 echo 'Install completed, check the README for next steps'
