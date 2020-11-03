6f8cf32d74db840c10b44a4ef9a50dca61a9b6cf
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-03 04:36:43 +0100

    update install.se

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 apt-get -y install `cat package.php`
 mv Ubuntu-20.04-fs_overlay.tar.bz2 /
 export MOVEBACKTO=`pwd`
-cd /
 tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
 mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
 cd "$MOVEBACKTO"
