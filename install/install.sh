6e2173af791a9ef9b20402f5760f93008eefdbdd
andreasviklund201 <58643050+andreasviklund201@users.noreply.github.com>
2020-11-23 19:24:29 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -26,10 +26,10 @@ systemctl restart php7.4-fpm
 systemctl restart nginx
 clear
 echo
-echo		#####################################################################
-echo		#   'Install completed, remember to create database for wordpress'  #
-echo		# 																	#
-echo		#																	#
-echo		# 'Edit /etc/nginx/sites-available/streamer.conf with your domain'  #
-echo		#																	#
-echo		#####################################################################
+echo		'######################################################################'
+echo		'#    Install completed, remember to create database for wordpress    #'
+echo		'# 																	  #'
+echo		'#																	  #'
+echo		'#  Edit /etc/nginx/sites-available/streamer.conf with your domain    #'
+echo		'#																	  #'
+echo		'######################################################################'
